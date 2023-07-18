282f69751b2b5745598ac025ba9a3e9a0a81e73b
jeremietec <jmwathani1@gmail.com>
2023-07-17 22:54:35 -0700

    the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,18 @@
 /**
  * main -prints the alphabet.
  *
- * return: Always 0 (success)
+ * Return: Always 0.
  */
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for(i = 0;i<26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
+
 }
