c3ea4f170a56b0167c0c8b1cb27168e61df9c24e
jeremietec <jmwathani1@gmail.com>
2023-07-19 17:23:47 -0700

    abs value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs _ computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  * @n: the int to check
  * Return: the absolute value of int
  */
